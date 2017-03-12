fe4e499d544d53a9bf628faaceb7e584013073d6
Stephen Florian <steezeven@gmail.com>
2017-03-11 22:38:05 -0700

    commented out ambitious goals and fixed mongoose syntax

--- db/models.js
+++ db/models.js
@@ -3,38 +3,37 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 
 mongoose.connect(process.env.MONGO_URL)
 
-const equipmentGroupSchema = mongoose.Schema({
-  type: String,
-  price: Number,
-  _equiptment: [{type: ObjectId, ref: 'Equiptment'}]
-})
+// const equipmentGroupSchema = mongoose.Schema({
+//   type: String,
+//   price: Number,
+//   _equiptment: [{type: ObjectId, ref: 'Equiptment'}]
+// })
 
 const equiptmentSchema = mongoose.Schema({
   condition: Number,
   size: Number,
   width: Number,
   brand: String
-  _equiptmentGroup: {type: ObjectId, ref: 'EquiptmentGroup'}
 })
 
-const customerSchema = mongoose.Schema({
-  name: String
-})
+// const customerSchema = mongoose.Schema({
+//   name: String
+// })
 
-const rentalSchema = mongoose.Schema({
-  rented: Date,
-  toBeReturned: Date,
-  returned: Date,
-  _customer: {type: ObjectId, ref: 'Customer'},
-  _equiptment: [{type: ObjectId, ref: 'Equiptment'}]
-})
+// const rentalSchema = mongoose.Schema({
+//   rented: Date,
+//   toBeReturned: Date,
+//   returned: Date,
+//   _customer: {type: ObjectId, ref: 'Customer'},
+//   _equiptment: [{type: ObjectId, ref: 'Equiptment'}]
+// })
 
 
 module.exports = {
 
-  EquiptmentGroup: mongoose.Model('EquiptmentGroup', equiptmentGroupSchema),
-  Equiptment: mongoose.Model('Equiptment', equiptmentSchema),
-  Customer: mongoose.Model('Customer', customerSchema),
-  Rental: mongoose.Model('Rental', rentalSchema)
+  // EquiptmentGroup: mongoose.Model('EquiptmentGroup', equiptmentGroupSchema),
+  Equiptment: mongoose.model('Equiptment', equiptmentSchema),
+  // Customer: mongoose.Model('Customer', customerSchema),
+  // Rental: mongoose.Model('Rental', rentalSchema)
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const models = require('./db/models.js')
+const models = require('../db/models.js')
 
 const EquiptmentGroup = models.EquiptmentGroup
 const Equiptment = models.Equiptment
